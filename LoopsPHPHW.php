17de817c5deac5f6d7ad7de1fd3985da5a602069
aryasuva2250 <as3526@njit.edu>
2020-09-21 18:57:55 -0400

    Loops PHP HW

---
+++ LoopsPHPHW.php
@@ -0,0 +1,11 @@
+//Arya Suva
+//IT202-009
+//PHPLoopsHW
+<?php
+	$arr1 = array(2,3,4,5,6,10,12,13,15);
+    foreach($arr1 as $number){
+    	if($number % 2 == 0){
+        	echo $number . " ";
+        }
+    }
+?>
