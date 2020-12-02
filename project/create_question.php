e6b157c6928fec764cd2d8e314762c1ce4fff0a5
aryasuva2250 <as3526@njit.edu>
2020-12-01 20:49:27 -0500

    changing create question file

--- project/create_question.php
+++ project/create_question.php
@@ -6,30 +6,44 @@ if (!has_role("User")) {
     die(header("Location: login.php"));
 }
 ?>
-    <h3>Create Question</h3>
-    <form method="POST">
-        <label>Question</label>
-        <input name="question" placeholder="Question"/>
-        <input type="submit" name="save" value="Create"/>
-    </form>
 <?php
-if (isset($_POST["save"])) {
-    //TODO add proper validation/checks
+if(isset($_GET["id"])){
+    $id = $_GET["id"];
+}
+?>
+<?php
+if(isset($_POST["save"])){
     $name = $_POST["question"];
     $user = get_user_id();
     $db = getDB();
     $stmt = $db->prepare("INSERT INTO Question (question, survey_id) VALUES(:name, :user)");
     $r = $stmt->execute([
-        ":name" => $name,
-        ":user" => $user
+        ":name"=>$name,
+	":user"=>$user,
     ]);
-    if ($r) {
+    if($r){
         flash("Created successfully with id: " . $db->lastInsertId());
     }
-    else {
+    else{
         $e = $stmt->errorInfo();
-        flash("Error creating: " . var_export($e, true));
+	flash("Error creating: " . var_export($e, true));
     }
 }
 ?>
+
+    <div class="createQuestion">
+    <h3>Create Question</h3>
+    </div>
+    <div class="createQForm">
+    <form method="POST">
+        <div class="createQ">
+	<label>Question</label>
+        <input name="question" placeholder="Question"/>
+	</div>
+	<div class="createQSubmit">
+        <input type="submit" name="save" value="Create"/>
+	</div>
+    </form>
+    </div>
+
 <?php require(__DIR__ . "/partials/flash.php");
